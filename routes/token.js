a57d354cbbcac395287bfc14b1a401ae0e9a4374
tbarry2 <tbact528@hotmail.com>
2023-02-16 23:27:16 -0500

    add tokens and user endpoint template

---
+++ routes/token.js
@@ -0,0 +1,30 @@
+var express = require("express");
+const validate = require("../validate");
+var router = express.Router();
+
+router.post("/refresh", function (req, res) {
+  validate.verifyRequest(req, res, (valRes) => {
+    const accessToken = validate.createAccessToken(valRes.username);
+    res.send(JSON.stringify({ accessToken: "Bearer " + accessToken }));
+  });
+});
+
+router.get("/login", function (req, res) {
+  var username = "user1"; //req.body.username.toLowerCase();
+  //TODO: Add username and password verification here
+  const accessToken = validate.createAccessToken(username);
+  const refreshToken = validate.createRefreshToken(username);
+  if (accessToken && refreshToken) {
+    res.send(
+      JSON.stringify({
+        text: "Login Successful",
+        accessToken: "Bearer " + accessToken,
+        refreshToken: "Bearer " + refreshToken,
+      })
+    );
+  } else {
+    res.send("Login Failed");
+  }
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/create', function (req, res, next) {
+  res.send("Create the User here");
+});
+
+router.get('/read', function (req, res, next) {
+  res.send("Get the User here");
+});
+
+router.post('/update', function (req, res, next) {
+  res.send("Update the User here");
+});
+
+router.post('/delete', function (req, res, next) {
+  res.send("Delete the User here");
 });
 
 module.exports = router;
