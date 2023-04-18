8b8db29b2ae5c2e654c60a5cc97fc10ea63c0346
tbarry2 <tbact528@hotmail.com>
2023-04-17 22:41:04 -0400

    Revert "put back to schemas"
    This reverts commit 1c2f0b9206b27cbceba5794e6150a75ff3b81cd6.

--- src/model/QuizzesDb.ts
+++ src/model/QuizzesDb.ts
@@ -1,5 +1,5 @@
 import { getConnection } from "./UtilsDb";
-import { Database as QuizzesSchema } from "../schemas/Quizzes";
+import { Database as QuizzesSchema } from "../schema/Quizzes";
 import { CronJob } from "cron";
 import { getCurrentNeo, setImageOfTheDay } from "./NeoDB";
 
